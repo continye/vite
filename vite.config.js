579f95df351800d3bf5d4c78712d747c1b44b647
contiyu <131341345+continye@users.noreply.github.com>
2024-09-26 16:32:40 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/vite',
+  publicPath: '/vite.github.io',
   server: {
     host: "0.0.0.0",
     hmr: true,
